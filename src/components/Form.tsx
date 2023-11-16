e38f791dac53e062a96a1ee0b17532eca94aef58
Amir Hossein Peyvand <amirhosseinpeyvand2020@gmail.com>
2023-11-16 09:39:27 -0800

    Building Form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,5 @@
+import "../sass/Form.scss";
+
 const Form = () => {
   return (
     <form className="form">
@@ -13,15 +15,17 @@ const Form = () => {
           placeholder="e.g. 1234 5678 9123 0000"
         />
       </div>
-      <div className="formGroup">
-        <div>
-          <label htmlFor="date">Exp. Date (MM/YY)</label>
+      <div className="lastOne">
+        <div className="left">
+          <label htmlFor="date">
+            Exp. Date <span>(MM/YY)</span>
+          </label>
           <div>
             <input type="text" id="date" placeholder="MM" />
             <input type="text" id="date" placeholder="YY" />
           </div>
         </div>
-        <div>
+        <div className="right">
           <label htmlFor="cvc">CVC</label>
           <input type="text" id="cvc" placeholder="e.g. 123" />
         </div>
